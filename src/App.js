1f27942745af02d282e27d7b830af8cdb4d890c3
harry <harsimrans329@gmail.com>
2020-01-04 22:53:10 -0600

    Feature [#MyRead 101] Books are filtered and displayed acc to their status.

--- src/App.js
+++ src/App.js
@@ -13,15 +13,26 @@ class App extends Component {
       this.setState({books: this.updatedbooks})
   }
 
+  getAllBookList() {
+        const reading = this.state.books.filter( book => book.shelf === "currentlyReading")
+        const read = this.state.books.filter( book => book.shelf === "read")
+        const wantToRead = this.state.books.filter( book => book.shelf === "wantToRead")
+        return {reading: reading, read: read, wantToRead: wantToRead}
+  }
     render() {
-    return(
-        <a>
-          <a className="header">
-            MyReads
-          </a>
-          <Shelf heading = "Currently Reading" bookList = {this.state.books}> </Shelf>  
-        </a>
-    )
+        const {reading, read, wantToRead} = this.getAllBookList();
+        console.log("I am coming from render")
+        console.log(reading)
+        return(
+            <a>
+              <a className="header">
+                MyReads
+              </a>
+              <Shelf heading = "Currently Reading" bookList = {reading}> </Shelf>
+              <Shelf heading = "Read" bookList = {read}> </Shelf>
+              <Shelf heading = "Want To Read" bookList = {wantToRead}> </Shelf>
+            </a>
+        )
   }
 
 }
--- src/Shelf.js
+++ src/Shelf.js
@@ -14,7 +14,11 @@ export default class Shelf extends Component {
             <a>
                 <a>{this.props.heading}</a>
                 <ol className="shelf-list">
-                    {this.props.bookList.map((book) => (<li key={ book.title} className="shelf-list-item"> <img src={book.imageLinks.smallThumbnail} className="shelf-list-img"></img>{book.title}</li>))}
+                    {this.props.bookList.map((book) => (<li key={ book.title} className="shelf-list-item">
+                        <img src={book.imageLinks.smallThumbnail} className="shelf-list-img"></img>
+                        {book.title}
+                        <button> clickMe </button>
+                    </li>))}
                 </ol>
             </a>
         )
